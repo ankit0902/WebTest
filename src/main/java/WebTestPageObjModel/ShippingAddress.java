7902a5c62aa01a646e4900729d2aeb525f4d6fda
ankit0902 <ankit.dhanorkar201@gmail.com>
2020-02-27 00:44:59 +0000

    new file

---
+++ src/main/java/WebTestPageObjModel/ShippingAddress.java
@@ -0,0 +1,17 @@
+package WebTestPageObjModel;
+
+import org.openqa.selenium.By;
+
+public class ShippingAddress extends Utils {
+
+        private By _continue = By.xpath("//input[@onclick='ShippingMethod.save()']");
+
+
+        public void userShippingAddress(){clickOnElement(_continue);
+
+        }
+    }
+
+
+
+
